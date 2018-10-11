49bb72378b0ece3b5ffef121b1fd708c9abac17c
CB_ATL_105_David_Shin <cb_atl_105_david_shin@CBs-MacBook-Air-9.local>
2018-10-11 12:47:48 -0400

    testing phase for the fucntion

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const alphabetButtons = document.getElementsByClassName("button");
+const images = document.getElementById("images");
+const inputHold = document.getElementById("inputHold");
+const categoryName = document.getElementById("categoryName");
+const answerArray = [];
+const alphabet = [
+ "a",
+ "b",
+ "c",
+ "d",
+ "e",
+ "f",
+ "g",
+ "h",
+ "i",
+ "j",
+ "k",
+ "l",
+ "m",
+ "n",
+ "o",
+ "p",
+ "q",
+ "r",
+ "s",
+ "t",
+ "u",
+ "v",
+ "w",
+ "x",
+ "y",
+ "z"
+];
+
+const words = [
+ "html",
+ "javascript",
+ "cascading style sheet",
+ "mario",
+ "luigi",
+ "browser"
+];
