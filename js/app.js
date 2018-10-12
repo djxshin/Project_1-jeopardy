ce11fc84e6f0eb5b78c3b89d62de97e7ccc1277d
CB_ATL_105_David_Shin <cb_atl_105_david_shin@CBs-MacBook-Air-9.local>
2018-10-12 15:58:15 -0400

    MVP function commit 2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-const dollar = document.querySelector("#dollar")
+let dollar = document.querySelector("#dollar")
+let zeroDollar = 0
+// 1Q variables ID=#box6
+let A = document.querySelector("#A")
+let B = document.querySelector("#B")
+let C = document.querySelector("#C")
+let D = document.querySelector("#D")
+
+
+C.addEventListener('click', function(){
+    zeroDollar += 10
+    dollar.innerHTML = zeroDollar
+    document.getElementsByClassName("btn")[0].style.opacity = "0"
+    document.getElementsByClassName("btn")[0].style.transition = "2s"
+})
+
