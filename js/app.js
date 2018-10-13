283de6d88a3870c33bc3263a32cbd611f45b1cad
CB_ATL_105_David_Shin <cb_atl_105_david_shin@CBs-MacBook-Air-9.local>
2018-10-12 20:05:01 -0400

    my friend rich came by and gave me pointers to DRY my simple code that woud've been 1000lines

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 let dollar = document.querySelector("#dollar")
 let zeroDollar = 0
+// how much we need to take away or add from score
+let score= 0
+// score value that stays
+
+
 // 1Q variables ID=#box6
 let A = document.querySelector("#A")
 let B = document.querySelector("#B")
@@ -7,10 +12,83 @@ let C = document.querySelector("#C")
 let D = document.querySelector("#D")
 
 
-C.addEventListener('click', function(){
-    zeroDollar += 10
-    dollar.innerHTML = zeroDollar
-    document.getElementsByClassName("btn")[0].style.opacity = "0"
-    document.getElementsByClassName("btn")[0].style.transition = "2s"
+// A.addEventListener('click', function(){
+//     zeroDollar -= 100
+//     dollar.innerHTML = zeroDollar
+//     document.getElementsByClassName("btn")[0].style.opacity = "0"
+//     document.getElementsByClassName("btn")[0].style.transition = "2s"
+    
+// })
+// B.addEventListener('click', function(){
+//     zeroDollar -= 100
+//     dollar.innerHTML = zeroDollar
+//     document.getElementsByClassName("btn")[0].style.opacity = "0"
+//     document.getElementsByClassName("btn")[0].style.transition = "2s"
+    
+// })
+// C.addEventListener('click', function(){
+//     zeroDollar += 100
+//     dollar.innerHTML = zeroDollar
+//     document.getElementsByClassName("btn")[0].style.opacity = "0"
+//     document.getElementsByClassName("btn")[0].style.transition = "2s"
+//     document.getElementsByClassName("btn")[0].removeAttribute('data-toggle')
+// })
+// D.addEventListener('click', function(){
+//     zeroDollar -= 100
+//     dollar.innerHTML = zeroDollar
+//     document.getElementsByClassName("btn")[0].style.opacity = "0"
+//     document.getElementsByClassName("btn")[0].style.transition = "2s"
+    
+// })
+
+
+
+$('.modal-btn').on('click', function(){
+    
+    if ($(this).hasClass('hundred')){
+        console.log('yea this is hundred')
+        zeroDollar = 100;
+   
+    } else if ($(this).hasClass('twoHundred')){
+        console.log('yea this is hundred')
+        zeroDollar = 200;
+   
+    }else if ($(this).hasClass('threeHundred')){
+        console.log('yea this is hundred')
+        zeroDollar = 300;
+   
+    }else if ($(this).hasClass('fourHundred')){
+        console.log('yea this is hundred')
+        zeroDollar = 400;
+   
+    }else if ($(this).hasClass('fiveHundred')){
+        console.log('yea this is hundred')
+        zeroDollar = 500;
+   
+    }
+})
+
+$('.modal-footer button').on('click', function(){
+    if ($(this).attr('data-attribute') == "right"){
+        // if these buttons = data attribute = right, we add 100pts
+        score += zeroDollar
+       
+    }
+    else{
+        // if else you would -100;
+        score -= zeroDollar
+    }
+    
+    dollar.innerHTML = score
+    // each time you click it'll update innerHTML whether right or wrong
+
+    // traverse (moving up the dom tree to find closet .modal class)
+    var gooey = $(this).closest('.modal').prev()
+    // then find prevous element which is the main button
+    gooey.css({
+        'opacity':'0',
+        'transition': '2000ms'
+    })
+    gooey.removeAttr('data-toggle');
 })
 
