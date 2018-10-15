5a8bec85973c1d6a3db7463516b203349cf5b908
CB_ATL_105_David_Shin <cb_atl_105_david_shin@CBs-MacBook-Air-9.local>
2018-10-15 00:54:36 -0400

    player2 JS. scores tally up

--- js/2 player.js
+++ js/2 player.js
@@ -4,68 +4,64 @@ let zeroDollar = 0;
 let score = 0;
 // score value that stays
 
-let question = document.querySelector('#question');
+let question = document.querySelector("#question");
 let zeroCount = 0;
 let counter = 0;
-
-
+let Player2 = 0;
 
 $(".modal-btn").on("click", function() {
   // .modal-btn which = classes for all 25 Q buttons; class onlick function
 
   if ($(this).hasClass("hundred")) {
     // if the button clicked aka (this) is '.hundred' class;
-    
+
     zeroDollar = 100;
     // let zeroDollar value is 100;
   } else if ($(this).hasClass("twoHundred")) {
-   
     zeroDollar = 200;
   } else if ($(this).hasClass("threeHundred")) {
-   
     zeroDollar = 300;
   } else if ($(this).hasClass("fourHundred")) {
-   
     zeroDollar = 400;
   } else if ($(this).hasClass("fiveHundred")) {
-    
     zeroDollar = 500;
-
   }
 });
 
-
-
 $(".modal-footer button").on("click", function() {
-  if ($(this).attr("data-attribute") == "right") {
+  if ($(this).hasClass("btn-primary")) {
+    zeroCount = 1;
+  }
+
+  counter += zeroCount;
+
+  if ($(this).attr("data-attribute") == "right" && counter % 2 == 1) {
     // if these buttons = data attribute = right, we add 100pts
     score += zeroDollar;
-  } else {
+  } else if ($(this).attr("data-attribute") == "wrong" && counter % 2 == 1) {
     // if else you would -100;
     score -= zeroDollar;
+  } else if ($(this).attr("data-attribute") == "right" && counter % 2 == 0) {
+    Player2 += zeroDollar
+  } else {
+    Player2 -= zeroDollar
   }
-
   dollar.innerHTML = score;
+  count.innerHTML = Player2;
   // each time you click it'll update innerHTML whether right or wrong
 
-  if($(this).hasClass('btn-primary')){
-      zeroCount = 1;
-     
+  if (dollar.innerHTML < 0) {
+    swal(
+      "you reached negative points 😕  no worries, you can continue playing but please be more cautious with your answers"
+    );
   }
 
- counter += zeroCount
- 
-count.innerHTML = counter;
-
-if(dollar.innerHTML < 0){
-    swal("you reached negative points 😕  no worries, you can continue playing but please be more cautious with your answers")
-}
-
-if (count.innerHTML > 19){
-
-   
-    swal("That's the Game! Thank you for playing! your final score is " + dollar.innerHTML)
-}
+  if (counter > 19) {
+    swal(
+      "That's the Game! Thank you for playing! your final score is " +
+        dollar.innerHTML
+    );
+  }
 
   // traverse (moving up the dom tree to find closet .modal class)
   var gooey = $(this)
@@ -77,9 +73,7 @@ if (count.innerHTML > 19){
     transition: "2000ms"
   });
   gooey.removeAttr("data-toggle");
-
 });
-
 
 // A.addEventListener('click', function(){
 //     zeroDollar -= 100
